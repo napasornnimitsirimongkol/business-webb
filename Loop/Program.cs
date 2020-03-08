eab8e1bfcebbda776c9d7dfc5497ee6da84a7e79
siamcodes <siamcodes@gmail.com>
2020-03-08 09:56:09 +0700

    first commit

---
+++ Loop/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, z;
+            Console.Write("Mutiply: ");
+            x = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 12; i++ )
+            {
+                z = x * i;
+                Console.WriteLine(x+" * "+i+" = "+z);
+            }
+        }
+    }
+}
